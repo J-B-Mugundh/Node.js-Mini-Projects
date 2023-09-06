43b42a5316580cfb94b803f5e04efa5257cf38e3
Mugundh <mugundhjb@gmail.com>
2023-09-06 20:16:40 +0530

    Global objects in Nodejs

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+// Global Objects
+
 //console.log("Hello World!");
 
-let count = 0;
-const timer = setInterval(() => {
-    count = count + 2;
-    console.log(`${count} seconds have passed`);
-    if(count > 6)
-        clearInterval(timer);
-}, 2000);
+// let count = 0;
+// const timer = setInterval(() => {
+//     count = count + 2;
+//     console.log(`${count} seconds have passed`);
+//     if(count > 6)
+//         clearInterval(timer);
+// }, 2000);
+
+// console.log(__dirname);
+
+//console.log(__filename);
