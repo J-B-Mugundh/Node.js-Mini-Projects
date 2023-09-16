74915bc5c6b6f3f971229fe8866ceed0a6e3c198
Mugundh <mugundhjb@gmail.com>
2023-09-16 16:14:48 +0530

    Readable stream in Nodejs

--- app.js
+++ app.js
@@ -49,13 +49,23 @@
 
 // Server in Nodejs
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    res.end('hello world');
-});
+// const server = http.createServer((req, res) => {
+//     console.log(req.url);
+//     res.writeHead(200, {'Content-type': 'text/plain'});
+//     res.end('hello world');
+// });
 
-console.log('port 3000');
-server.listen(3000, '127.0.0.1');
+// console.log('port 3000');
+// server.listen(3000, '127.0.0.1');
+
+// Readable stream in Nodejs
+
+const fs = require('fs');
+
+const readStream = fs.createReadStream(__dirname + '\\README.md');
+
+readStream.on('data', (chunk) => {
+    console.log('data read => \n' + chunk);
+})
