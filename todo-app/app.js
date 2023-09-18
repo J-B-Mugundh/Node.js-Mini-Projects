9c140e229c7f5826de3dfa6aec71d6e7e1de27f1
Mugundh <mugundhjb@gmail.com>
2023-09-18 18:04:24 +0530

    Add and Delete Todo

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
-
 const todoController = require('./controllers/todoController');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public')); // middleware
+app.use(express.json()); // Parse JSON requests
 
-//controllers
+// Controllers
 todoController(app);
 
-app.listen(3000);
-
-console.log("You're listening to port 3000");
+app.listen(3000, () => {
+    console.log("You're listening to port 3000");
+});
--- todo-app/controllers/todoController.js
+++ todo-app/controllers/todoController.js
@@ -1,13 +1,33 @@
+let data = [{item: 'Read NodeJS'}, {item : 'Go shopping'}, {item: 'Do workout'}];
+
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false});
+
 module.exports = (app) => {
     app.get('/todo', (req, res) => {
-        res.render('todo'); // render the view (ie the ejs files) to the user
-    })
+        res.render('todo', {todos: data}); // render the view (ie the ejs files) to the user
+    });
 
-    app.post('/todo', (req, res) => {
-        
-    })
+    app.post('/todo', urlencodedParser, (req, res) => {
+        try {
+            const newItem = { item: req.body.item }; // Create a new item object
+            data.push(newItem); // Adding the new todo data
+            // Then, we will have to send the data back to the front end
+            // This can be done using res.json(data)
+            res.json(data);
 
-    app.delete('/todo', (req, res) => {
-        
-    })
+        } catch (error) {
+            console.error('Error adding todo:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    });
+
+    app.delete('/todo/:item', (req, res) => {
+        const itemToDelete = req.params.item; // Get the item to delete from the URL parameter
+        data = data.filter((todo) => todo.item !== itemToDelete); // Filter out the item to delete
+        res.json(data);
+    });
+       
+    
+
 }
