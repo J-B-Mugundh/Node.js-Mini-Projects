510b234e77e3dfc44f7c064203f5ae2f42a6e042
Mugundh <mugundhjb@gmail.com>
2023-09-18 16:27:15 +0530

    Views in NodeJS

--- todo-app/controllers/todoController.js
+++ todo-app/controllers/todoController.js
@@ -1,6 +1,6 @@
-modules.export = (app) => {
+module.exports = (app) => {
     app.get('/todo', (req, res) => {
-
+        res.render('todo'); // render the view (ie the ejs files) to the user
     })
 
     app.post('/todo', (req, res) => {
